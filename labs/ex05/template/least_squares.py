f425b286de87ce30733a9c5f2ca0bee872a8a5a5
Marc-Egli <marc99.egli@gmail.com>
2020-10-29 10:24:04 +0100

    ex5

--- labs/ex05/template/least_squares.py
+++ labs/ex05/template/least_squares.py
@@ -14,4 +14,17 @@ def least_squares(y, tx):
     # least squares: TODO
     # returns mse, and optimal weights
     # ***************************************************
-    raise NotImplementedError
+    w = np.linalg.solve(tx.T@tx,tx.T@y)
+    return compute_loss(y,tx,w), w
+
+
+def compute_loss(y, tx, w):
+    """Calculate the loss.
+
+    You can calculate the loss using mse or mae.
+    """
+    # ***************************************************
+    # INSERT YOUR CODE HERE
+    # TODO: compute loss by MSE / MAE
+    # ***************************************************
+    return np.sum(np.power(y - tx@w,2))/len(y)
